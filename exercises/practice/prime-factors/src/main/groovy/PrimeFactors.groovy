8cfea65eefecdb8ddabf54ccd8af1ac5e1349530
kgun0521 <kavishka.gunathilaka@sysco.com>
2024-10-28 17:05:16 +0530

    Solved challenges

--- exercises/practice/prime-factors/src/main/groovy/PrimeFactors.groovy
+++ exercises/practice/prime-factors/src/main/groovy/PrimeFactors.groovy
@@ -1,6 +1,21 @@
 class PrimeFactors {
 
     static factors(value) {
-        throw new UnsupportedOperationException('Method implementation is missing')
+        if (value == 1) {
+            return []
+        }
+        long val = value
+        long i = 2
+        def factorsVals = []
+        while (true) {
+            if (val%i == 0) {
+                factorsVals.add(i)
+                val /= i
+                factorsVals = factorsVals.plus(factors(val))
+                break
+            }
+            i++
+        }
+        return factorsVals
     }
 }
